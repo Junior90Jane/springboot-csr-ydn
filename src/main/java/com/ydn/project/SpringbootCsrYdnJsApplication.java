1e379519c6782c9811a46390589f3ed83df24222
Junior90Jane <justjunior2024@gmail.com>
2024-11-25 13:50:06 +0800

    firstjs

---
+++ src/main/java/com/ydn/project/SpringbootCsrYdnJsApplication.java
@@ -0,0 +1,13 @@
+package com.ydn.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCsrYdnJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCsrYdnJsApplication.class, args);
+	}
+
+}
